81e8a50041eab63864753a7fc262cb9d32972e28
Edward23-ux <edwardcampos46@gmail.com>
2026-04-23 01:00:02 -0500

    Se modificó application properties

--- BackEnd/src/main/java/clinica/com/example/clinicaocupacional/Repository/PacienteRepository.java
+++ BackEnd/src/main/java/clinica/com/example/clinicaocupacional/Repository/PacienteRepository.java
@@ -2,6 +2,8 @@ package clinica.com.example.clinicaocupacional.Repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import clinica.com.example.clinicaocupacional.Model.Paciente;
+
 public interface PacienteRepository extends JpaRepository<Paciente, Long>{
 
 }
