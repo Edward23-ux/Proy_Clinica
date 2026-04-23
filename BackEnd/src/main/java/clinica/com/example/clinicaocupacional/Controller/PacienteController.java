6729083528bccf3511b752acdbe22009fefda1d9
gabo-0312 <u21209609@utp.edu.pe>
2026-04-23 00:41:40 -0500

    Se creo el PacienteController

---
+++ BackEnd/src/main/java/clinica/com/example/clinicaocupacional/Controller/PacienteController.java
@@ -0,0 +1,52 @@
+package clinica.com.example.clinicaocupacional.Controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import clinica.com.example.clinicaocupacional.Model.Paciente;
+import jakarta.validation.Valid;
+
+
+@Controller
+@RequestMapping("/pacientes")
+public class PacienteController {
+
+    @Autowired
+    private PacienteService pacienteService;
+    
+    @GetMapping("/dashboard")
+    public String dashboard(Model model) {
+        model.addAttribute("pacientes", pacienteService.obtenerTodos());
+        return "dashboard";
+    }
+
+    @GetMapping("/registro")
+    public String mostrarFormulario(Model model) {
+        if(!model.containsAttribute("paciente")){
+            model.addAttribute("paciente", new Paciente());
+        }
+        return "registro"; 
+    }
+    
+    @PostMapping("/guardar")
+    public String guardar(@Valid @ModelAttribute Paciente paciente, BindingResult result, RedirectAttributes flash){
+        if(result.hasErrors()){
+            return "registro";
+        }
+        try{
+            pacienteService.guardar(paciente);
+            flash.addFlashAttribute("success", "Paciente registrado con éxito");
+            return "redirect:/pacientes/registro";
+        }catch(IllegalArgumentException e){
+            flash.addFlashAttribute("error", e.getMessage());
+            return "redirect:/pacientes/registro";        
+        }
+    }
+    
+}
